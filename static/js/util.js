7ec74406f9e317641bc00db3dd577c24ab580ff2
Doug Watson <dougwatson@gocoder.io>
2023-02-06 16:23:39 -0600

    m

--- static/js/util.js
+++ static/js/util.js
@@ -5,7 +5,7 @@ checkUser()
 function checkUser(){
     // Read all cookies as an array of strings
     var cookies = document.cookie.split(';');
-    console.log("cookies=",cookies)
+    console.log("cookies=================================",cookies)
     // Iterate over the cookies
     for (var i = 0; i < cookies.length; i++) {
         // Trim leading whitespace from the cookie string
